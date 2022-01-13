baf518a63ce3fafa99a06e40d1982351011b03d4
Soros <s4650729@gmail.com>
2022-01-13 13:48:35 +0200

    logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,12 +1,18 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    public static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("forward to users");
         request.getRequestDispatcher("users.jsp").forward(request, response);
 //        response.sendRedirect("users.jsp");
     }
