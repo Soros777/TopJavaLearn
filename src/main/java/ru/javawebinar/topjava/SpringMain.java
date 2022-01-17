79cff1d518245c7d0df146ac106bf1e4b3b9160e
Soros <s4650729@gmail.com>
2022-01-17 18:50:38 +0200

    annotation processing

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,20 +4,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.UserRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-
-//        InMemoryUserRepository userRepository = (InMemoryUserRepository) appCtx.getBean("inMemoryUserRepository");
-//        InMemoryUserRepository userRepository = appCtx.getBean(InMemoryUserRepository.class);
-//        userRepository.getAll();
-        UserService userService = appCtx.getBean(UserService.class);
-        userService.create(new User(1, "name", "email", "password", Role.ADMIN));
-        appCtx.close();
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("All spring beans: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            UserRestController adminController = appCtx.getBean(UserRestController.class);
+            adminController.create(new User(null, "name", "email", "password", Role.ADMIN));
+        }
     }
 }
